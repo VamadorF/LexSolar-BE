9ac9d4a7ea8beb00aa41765e6ea86fb9460c2f35
Javier Fernandez <javier.fernandez@mejoradigital.cl>
2025-10-25 21:02:45 -0300

    feat: cors

--- src/installation/installation.service.ts
+++ src/installation/installation.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class InstallationService {}
+export class InstallationService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    // Create installation logic here
+    async createInstallation(data: any): Promise<any> {
+        // Example logic to create an installation record
+        const installation = await this.prismaService.installation.create({
+            data,
+        });
+        return installation;
+    }
+}
